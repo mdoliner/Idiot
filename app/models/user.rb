35e0ba78cb8fd1676f74d3f04b2dedf676716f11
Michael Doliner <msd83@cornell.edu>
2015-01-07 10:25:02 -0500

    Adds User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,34 @@
 class User < ActiveRecord::Base
+  USER_LEVELS = ["whitehat", "editor", "moderator", "regulator", "verified"]
+
+  validates :username, :password_digest, :email, :level, :session_token, presence: true
+  validates :username, :email, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :level, inclusion: {in: USER_LEVELS}
+
+  attr_reader :password
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
 end
