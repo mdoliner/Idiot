ab253a21729bd1a79a9275e927ebd92e1042b8ed
Michael Doliner <msd83@cornell.edu>
2015-01-10 18:30:44 -0500

    Orders improvements by date.

--- app/assets/javascripts/collections/improvements.js
+++ app/assets/javascripts/collections/improvements.js
@@ -1,3 +1,7 @@
 Idiot.Collections.Improvements = Backbone.Collection.extend({
-  urlRoot: "api/improvements"
+  urlRoot: "api/improvements",
+
+  comparator: function (improvement1, improvement2) {
+    return improvement1.get("created_at") < improvement2.get("created_at")
+  }
 });
